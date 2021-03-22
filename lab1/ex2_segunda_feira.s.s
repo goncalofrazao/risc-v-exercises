352cb739a7a5c94245d92e842eb12448e9214fdd
StrangeRabbit <valorantbeastnew@gmail.com>
2021-03-22 15:35:39 +0000

    correction ex 2

--- lab1/ex2_segunda_feira.s.s
+++ lab1/ex2_segunda_feira.s.s
@@ -1,18 +1,33 @@
+#----------enunciado----------
+#Determinar a maior potencia
+#de 2 menor que u(10)
+#u(10) e o 10º elemento
+#da sequencia de fibonacci
+
 .data
 x: .word 2
+
 
 .text
 la x15, x
 lw x10, x
-li x11, 10
-li x12, 2
-while:
-addi x15, x15, 4
-slli x10, x10, 1
-sw x10, 0(x15) 
+li x12, 55
 
-addi x12, x12, 1
-blt x12, x11, while
+for:
+blt x10, x12, this
+bgt x10,x12, otherwise
+
+otherwise:
+srli x10,x10,1
+j end
+
+this:
+slli x10, x10, 1
+j for
+
+end:
+addi x15, x15, 4
+sw x10, 0(x15) 
 
 li x17,1
 ecall
