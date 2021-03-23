136c6db6cb4d7b8c00439580a57e86571434e1f1
StrangeRabbit <valorantbeastnew@gmail.com>
2021-03-23 19:48:55 +0000

    fib sequence numeros pares added

---
+++ lab1/ex2_pares_fib.s
@@ -0,0 +1,42 @@
+#----------Enunciado----------
+# sequencia de fibonacci mas
+# so guarda na memoria os no.
+# impares
+
+
+.data
+x: .word 1
+y: .word 1
+
+.text
+la x10, y
+li x11, 20
+li x12, 2
+lw x14, x
+lw x15, y
+li x16, 1
+li x17, 1
+
+while:
+
+add x13, x0, x14
+add x14, x0, x15
+
+add x15, x14, x13
+
+
+
+addi x12, x12, 1
+andi x16, x12, 1
+beq x16, x17, while
+
+sw x15, 0(x10)
+addi x10, x10, 4
+
+blt x12, x11, while
+
+li x17,1
+ecall
+
+li x17,10
+ecall
